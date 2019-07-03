4bd23ec1ceb9cac81b676c3a36e5d4b01c81bfb1
Kelly Lu <37441523+Kelly2336@users.noreply.github.com>
2019-07-03 16:44:04 +0800

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,8 +1,4 @@
-void Union(int x, int y)
+bool sameSet(int x, int y)
 {
-    int px = Find(x);
-    int py = Find(y);
-    if( rnk[px]>rnk[py] ) par[py] = px;  
-    else if( rnk[px]<rnk[py] ) par[px] = py;
-    else par[px] = py, rnk[py]++;
+    return Find(x) == Find(y);
 }
