7a430db0204053c062c691c80fb57b9f80e2fea1
Kelly Lu <37441523+Kelly2336@users.noreply.github.com>
2019-07-03 16:39:52 +0800

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,10 +1,8 @@
-int par[maxn];
-int rnk[maxn];
-int init(int n)
+// 找x屬於哪一個集合
+// 順便把x直接接到集合代表的下面
+// 複雜度是樹的高度，所以邊做邊把樹壓平
+int Find(int x)
 {
-    for(int i=0; i<n; i++)
-    {
-        par[i] = i;
-        rnk[i] = 1;
-    }
+    if( x != par[x] ) par[x] = Find(par[x]);
+    return par[x];
 }
