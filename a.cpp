413bd43a19b155f591428bd5449752524360c8a4
Kelly Lu <37441523+Kelly2336@users.noreply.github.com>
2019-07-03 16:51:20 +0800

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,8 +1,6 @@
+array版
+int key = 3;
 int a[5] = {1,2,5,7,9};
-int* pos = lower_bound( a, a+n, key );
-vector<int> v (a, a+5);
-vector<int>::iterator iter;
-iter = lower_bound( v.begin(), v.end(), key );
-int value = *iter;    // value = *pos;
-return 的型態是指標/iterator，代表找到的位置
-如果找不到，會回傳容器尾端的位置.end()
+int* pos = lower_bound(a,a+n,key);
+cout << *pos << endl;
+//5
